c03ea9abbc277c30abbab12b81831d0cbceb143e
k18083kk <k18083kk@pck18083kk.local>
2020-12-20 14:59:14 +0900

    raspberry-notify2

--- RaspberryNotify.py
+++ RaspberryNotify.py
@@ -3,43 +3,42 @@ import time
 import requests
 import schedule
 import RPi.GPIO as GPIO
-import picamera
-
-
+import numpy as np
+import cv2 as cv
+​
 # ピン番号指定
 gpio_led = 17
 gpio_sw = 26
 # GPIO指定します
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 # チャンネルを設定します
 GPIO.setup(gpio_sw,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(gpio_led,GPIO.OUT)
-
+​
 # 画像が保存されるパス
 CAM_DIR ="/home/pi/Web-IoT-/"
-camera = picamera.PiCamera()
 
+cap = cv.VideoCapture(0)
+​
+​
 def line_notify_post():
     """Line Notify Api"""
     # トークンID
-    line_notify_token = "V30inRo2W7u9Tvxb7Mfh7r0eqIBZY3FywmDNUa6kEJZ"
+    line_notify_token = "自分のトークンID"
     # LINE Notify APIのURL
     line_notify_api = "https://notify-api.line.me/api/notify"
     # Notify URL
     headers = {'Authorization': 'Bearer ' + line_notify_token}
-
-    # 画像が保存されるディレクトリのパス
-    # images = "/home/pi/Web-IoT-/"
-
+​
     # 画像の読み込み
     files = {'imageFile': open(CAM_DIR + "in_fridge.jpg","rb")}
-
+​
     # message内容
     text = '画像取得'
     payload = {'message': text}
-
+​
     requests.post(line_notify_api, data=payload, headers=headers, files=files)
-
+​
 # カメラ撮影
 def camera_func(x):
     if GPIO.input(gpio_sw) == 0:
@@ -47,9 +46,12 @@ def camera_func(x):
         save_dir_filename = CAM_DIR + filename
         GPIO.output(gpio_led, 1)
         time.sleep(2)
-        camera.capture(save_dir_filename)
+        # camera.capture(save_dir_filename)
+		ret, frame = cap.read()
+		cv.imwrite(filename, frame)
+​
         GPIO.output(gpio_led, 0)
-
+​
 GPIO.add_event_detect(gpio_sw, GPIO.FALLING, callback=camera_func)
 times = ["01:38", "01:39"]
 schedule.every().day.at(times[0]).do(line_notify_post)
@@ -58,6 +60,6 @@ try:
     while True:
         schedule.run_pending()
         time.sleep(1)
-
+​
 except KeyboardInterrupt:
     GPIO.cleanup()
